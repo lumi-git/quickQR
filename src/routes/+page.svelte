2f4a45ea58f49151ce9c919fda62f46de4a4fa0d
ronan tremoureux <ronan.tremoureux@gmail.com>
2023-11-06 11:00:41 +0100

    changing css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
 <script>
 
     import { onMount } from 'svelte';
+    import { scale } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
     function updateQRCode() {
         
         var qrtext = document.getElementById("qrtext").value;
@@ -24,19 +26,20 @@
 </script>
 
 <div class = "text-center">
-    <p class ="text-5xl font-bold text-white py-9">Welcome to quickQR</p>
+    <p class ="text-5xl font-bold text-white my-9">Welcome to quickQR</p>
 </div>
 
-<div class = "block py-16">
+<div class = "block my-16">
 
-    <div class=" flex">
-        <p class=" m-auto text-4xl font-bold text-white py-9 ">Enter text to generate QR code</p>
+    <div class=" flex text-center">
+        <p class=" m-auto sm:text-4xl text-2xl font-bold text-white py-9 ">Enter text to generate QR code</p>
     </div>
 
     <div class=" flex">
-        <input class = "m-auto p-3  bg-green-300  text-5xl" type="text" id="qrtext"  placeholder="your text" />
+        <input class = "typingbar  mx-auto p-3 text-center placeholder:text-white hover:scale-105" type="text" id="qrtext"  placeholder="your text" />
     </div>
 
 </div>
 
-<img class = "m-auto" id="qrcode" src="https://quickchart.io/qr?text= " alt="QR Code" />
+<img class = "m-auto hover:scale-[2]" id="qrcode" src="https://quickchart.io/qr?text= " alt="QR Code" />
+
